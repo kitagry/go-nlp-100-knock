8c1683916424a4ca8a65b9563e729748393beca7
kitagawa <kitadrum50@gmail.com>
2019-07-03 00:55:11 +0900

    Done ex13

--- ex13/main.sh
+++ ex13/main.sh
@@ -2,7 +2,6 @@
 
 wget http://www.cl.ecei.tohoku.ac.jp/nlp100/data/hightemp.txt
 cut -f 1 -d '	' hightemp.txt > col1.txt
-cat col1.txt
 cut -f 2 -d '	' hightemp.txt > col2.txt
-cat col2.txt
+paste -d '	' col1.txt col2.txt
 rm hightemp.txt col1.txt col2.txt
