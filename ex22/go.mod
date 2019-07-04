5083f32b5b6a39c15dfbab3e22680a7c0166b6e5
kitagawa <kitadrum50@gmail.com>
2019-07-04 23:43:51 +0900

    Done ex22

---
+++ ex22/go.mod
@@ -0,0 +1,3 @@
+module github.com/kitagry/go-nlp-100-knock/ex22
+
+go 1.12
---
+++ ex22/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Article struct {
+	Title string `json:"title"`
+	Text  string `json:"text"`
+}
+
+func main() {
+	file, err := os.Open("../jawiki-country.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	sc := bufio.NewScanner(file)
+	buf := make([]byte, 10000)
+	sc.Buffer(buf, 1000000)
+
+	var article Article
+	for sc.Scan() {
+		var art Article
+		if err := json.Unmarshal(sc.Bytes(), &art); err != nil {
+			fmt.Println(err)
+			return
+		}
+		if art.Title == "イギリス" {
+			article = art
+		}
+	}
+
+	stReader := strings.NewReader(article.Text)
+	sc = bufio.NewScanner(stReader)
+	for sc.Scan() {
+		t := sc.Text()
+		if strings.Contains(t, "Category") {
+			fmt.Println(t[11 : len(t)-2])
+		}
+	}
+}
