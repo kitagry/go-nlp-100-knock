6420e02665e99a05fd505a1c695543a4737f5bac
kitagawa <kitadrum50@gmail.com>
2019-07-07 23:28:13 +0900

    Done ex29

---
+++ ex29/go.mod
@@ -0,0 +1,3 @@
+module github.com/kitagry/go-nlp-100-knock/ex29
+
+go 1.12
---
+++ ex29/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type Article struct {
+	Title string `json:"title"`
+	Text  string `json:"text"`
+}
+
+func main() {
+	file, err := os.Open("../jawiki-country.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	sc := bufio.NewScanner(file)
+	buf := make([]byte, 10000)
+	sc.Buffer(buf, 1000000)
+
+	var article Article
+	for sc.Scan() {
+		var art Article
+		if err := json.Unmarshal(sc.Bytes(), &art); err != nil {
+			fmt.Println(err)
+			return
+		}
+		if art.Title == "イギリス" {
+			article = art
+		}
+	}
+
+	strReader := strings.NewReader(article.Text)
+	sc = bufio.NewScanner(strReader)
+
+	var imageName string
+	reg := regexp.MustCompile(`\|国旗画像 = (.*)`)
+	for sc.Scan() {
+		t := sc.Text()
+		if reg.MatchString(t) {
+			imageName = reg.ReplaceAllString(t, "$1")
+		}
+	}
+
+	requestUrl := fmt.Sprintf("https://www.mediawiki.org/w/api.php?action=query&format=json&prop=imageinfo&titles=File:%s&iiprop=url", strings.ReplaceAll(imageName, " ", "%20"))
+	res, err := http.Get(requestUrl)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var resp map[string]interface{}
+	json.Unmarshal(body, &resp)
+
+	fmt.Println(resp["query"].(map[string]interface{})["pages"].(map[string]interface{})["-1"].(map[string]interface{})["imageinfo"].([]interface{})[0].(map[string]interface{})["url"])
+}
